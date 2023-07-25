9a883bb461bdac0b20d5d9688df8b306ba5ed445
Felipe Silva <felipe.allware@gmail.com>
2023-07-24 22:48:59 -0300

    Ajustando base ref do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/pages/home/home.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
